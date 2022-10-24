ed4ccf87756a64275a79cbe54179d5dbb12594e8
grsagor <grsagor08@gmail.com>
2022-10-25 00:46:15 +0600

    api created for courses

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const courses = require('./data/courses.json');
+
 app.get('/', (req, res) => {
     res.send('running');
-})
+});
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
 
 app.listen(port, () => {
     console.log('running successfully');
-})
+});

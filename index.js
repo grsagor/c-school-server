3f8dfab1067fd5a210713c8c8b7f479035c18270
grsagor <grsagor08@gmail.com>
2022-10-25 01:11:34 +0600

    api created for each course

--- index.js
+++ index.js
@@ -8,12 +8,18 @@ app.use(cors());
 const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
-    res.send('running');
+    res.send('runnings');
 });
 
 app.get('/courses', (req, res) => {
     res.send(courses);
 });
+
+app.get('/course/:name', (req, res) => {
+    const name = req.params.name;
+    const getCourse = courses?.filter(course => course.name == name);
+    res.send(getCourse);
+})
 
 app.listen(port, () => {
     console.log('running successfully');

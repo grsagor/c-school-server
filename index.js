847ae5a7a2b12cc27f8ea7f64448b8f6c7a212af
grsagor <grsagor08@gmail.com>
2022-10-27 22:55:26 +0600

    finally updated perfectly

--- index.js
+++ index.js
@@ -19,7 +19,13 @@ app.get('/course/:name', (req, res) => {
     const name = req.params.name;
     const getCourse = courses?.filter(course => course.name == name);
     res.send(getCourse);
-})
+});
+
+app.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const getCourse = courses?.filter(course => course.id == id);
+    res.send(getCourse);
+});
 
 app.listen(port, () => {
     console.log('running successfully');
